889328c332a5ed36c7ae6e6f23ff2d777f3cb6c3
tyelf22 <tyelf22@hotmail.com>
2021-06-29 12:26:39 -0600

    caesar cipher alogrithim finished

---
+++ main.js
@@ -0,0 +1,37 @@
+const solveBtn = document.querySelector('#solveBtn')
+solveBtn.addEventListener("click", decoder)
+
+
+//const givenKey = "FKMUHRJCQEITHK"
+//const givenSentence = "For where your treasure is there your heart will also be"
+const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+
+function decoder(key, sentence){
+    console.log('connected')
+    sentence = document.querySelector('#sentenceTxt').value
+    key = document.querySelector('#keyTxt').value
+    let answerTxt = document.querySelector('#answerTxt')
+
+  let answer = []
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  let splitKey = key.toLowerCase().split('')
+  let combined = sentence.replace(regex, '').toLowerCase().split(' ').join('').split('')
+  let uniq = [...new Set(combined)];
+  alphabet.forEach(letter => {
+    if(!uniq.includes(letter)){
+      uniq.push(letter)
+    }
+  })
+  splitKey.forEach(letter => {
+    let indexOfUniqSentence = uniq.indexOf(letter)
+    answer.push(alphabet[indexOfUniqSentence])
+    
+  })
+
+
+  answerTxt.innerHTML = answer.join('')
+
+  if(answerTxt.textContent == "asthecrowflies"){
+      answerTxt.style.color = 'lightGreen'
+  }
+}
